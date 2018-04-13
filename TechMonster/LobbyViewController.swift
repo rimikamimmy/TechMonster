905659cf6fb365c79b92b79514ed39b2b24fa527
RIMIKA <rimikamimmy727@gmail.com>
2018-04-13 22:53:10 +0900

    敵が自動で回復するためのコードを間違えずに打つのが大変でした。 それぞれの動作によって、こんなにも打つコードが違うのかと改めて感じた。

---
+++ TechMonster/LobbyViewController.swift
@@ -0,0 +1,39 @@
+//
+//  LobbyViewController.swift
+//  TechMonster
+//
+//  Created by Rimika Inoguchi on 2018/04/13.
+//  Copyright © 2018年 Rimika Inoguchi. All rights reserved.
+//
+
+import UIKit
+
+class LobbyViewController: UIViewController {
+
+    var maxStamina: Float = 100
+    var sramina : Float = 100
+    varplayer : Float = 100
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
